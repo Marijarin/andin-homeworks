14136febf4fe39709738fa90775067a8882a5781
Марина Жаринова <phyrebirrd@gmail.com>
2022-11-24 21:57:58 +0500

    async funs implemented

--- app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/nmedia/repository/PostRepository.kt
@@ -1,13 +1,17 @@
 package ru.netology.nmedia.repository
 
-import androidx.lifecycle.LiveData
 import ru.netology.nmedia.dto.Post
+import java.lang.Exception
 
 interface PostRepository {
-    fun getAll(): List<Post>
-    /*fun getById(id: Long): Post*/
-    fun likeById(id: Long): Post
-    fun unlikeById(id: Long): Post
-    fun save(post: Post)
-    fun removeById(id: Long)
+    fun getAll(callback: Callback <List<Post>>){}
+    fun likeById(id: Long, callback: Callback <Post>)
+    fun unlikeById(id: Long, callback: Callback <Post>)
+    fun save(post: Post, callback: Callback <Post>)
+    fun removeById(id: Long, callback: Callback <Unit>)
+
+    interface Callback <T>{
+        fun onSuccess(value: T){}
+        fun onError(e: Exception){}
+    }
 }
